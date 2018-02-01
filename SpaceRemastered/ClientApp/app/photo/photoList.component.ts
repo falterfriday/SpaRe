cb290fdb513e18d5d4b4c012b8f4bcdc16752e11
Patrick Todd <patrick.todd@reisystems.com>
2018-01-31 19:00:56 -0500

    added css. added photolist class

--- SpaceRemastered/ClientApp/app/photo/photoList.component.ts
+++ SpaceRemastered/ClientApp/app/photo/photoList.component.ts
@@ -1,5 +1,21 @@
 ﻿import { Component, OnInit } from "@angular/core";
-
-//TODO create dataservice
-//import { DataService } from "../shared/dataService";
+import { DataService } from "../shared/dataService";
 import { Photo } from "../shared/photo";
+
+@Component({
+	selector: "photo-list",
+	templateUrl: "photoList.component.html",
+	styleUrls: ["photoList.component.css"]
+})
+
+export class PhotoList implements OnInit {
+	public photos: Photo[];
+
+	constructor(private data: DataService) {
+		this.photos = data.photos;
+	}
+
+	ngOnInit(): void {
+		this.data.loadPhotos().subscribe(() => this.photos = this.data.photos);
+	}
+}
